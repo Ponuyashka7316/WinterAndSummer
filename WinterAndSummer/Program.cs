f78c70d80f658efc108ffe5efa27f29553f3d6c3
wtwet <yuriygurin1@mail.ru>
2018-06-15 16:18:07 +0300

    Initial commit

--- WinterAndSummer/Program.cs
+++ WinterAndSummer/Program.cs
@@ -10,6 +10,12 @@ namespace WinterAndSummer
     {
         static void Main(string[] args)
         {
+            int[] testData1 = { 5, -2, 3, 8, 6 };
+            int[] testData2 = { -5, -5, -5, -42, 6, 12 };
+            Solution sln = new Solution();
+            Console.WriteLine(sln.WinterAndSummer(testData1));
+            Console.WriteLine(sln.WinterAndSummer(testData2));
+            Console.ReadKey();
         }
     }
 }
---
+++ WinterAndSummer/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterAndSummer
+{
+    class Solution
+    {
+        public int WinterAndSummer(int[] T)
+        {
+
+            for (int i = 1; i < T.Length - 1; ++i)
+            {
+                bool flag = true;
+                var subArray1 = T.SubArray(0, i);
+                var subArray2 = T.SubArray(i, T.Length - i);
+                if (subArray1.Length == 0 || subArray2.Length == 0)
+                    flag = false;
+                for (int j = 0; j < subArray1.Length; j++)
+                {
+                    for (int k = 0; k < subArray2.Length; k++)
+                    {
+
+                        if (subArray1[j] > subArray2[k])
+                            flag = false;
+
+                    }
+
+                }
+                if (flag)
+                    return i;
+            }
+            return T.Length;
+        }
+    }
+
+}
